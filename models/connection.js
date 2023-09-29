b496da51416d827137dc7bf5dcf4b2ce583bc094
PhilLeGoff <webbyphil65@gmail.com>
2023-09-29 13:49:45 +0200

    add entire project

---
+++ models/connection.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const connectionString = 'mongodb+srv://pgoff:5mLFn9bXYcmEV2Vr@cluster0.df20yl1.mongodb.net/blackboard?retryWrites=true&w=majority';
+
+mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
+  .then(() => console.log('Database connected'))
+  .catch(error => console.error(error));
